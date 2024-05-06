f28e50b761af4144b50742b8e2225e54f2a767be
rizanad <adhikaririzan9@gmail.com>
2024-05-06 08:05:33 +0545

    feat: add express js

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+const name = "Texas";
+
+// name="another Texas";
+
+const studentObject = {
+    collegeName:"Texas",
+    age:20
+}
+
+console.log(studentObject.collegeName);
+
+
